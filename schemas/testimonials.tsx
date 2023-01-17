c1ffcce51cecace482c7259b9ad12cfb84922d80
Kweayon  Clark <Kweayon@gmail.com>
2023-01-17 15:28:26 -0600

    Testimonials Document

---
+++ schemas/testimonials.tsx
@@ -0,0 +1,31 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+    name: "testimonials",
+    title: "Testimonials",
+    type: "document",
+    fields: [
+        defineField({
+            name: "name",
+            title: "Name",
+            type: "string"
+        }),
+        defineField({
+            name: "company",
+            title: "Company",
+            type: "string"
+        }),
+        defineField({
+            name: "imgUrl",
+            title:"ImgUrl",
+            type:"image",
+            options: {
+                hotspot: true
+            }}),
+        defineField({
+            name: "feedback",
+            title:"Feedback",
+            type:"string"
+        })
+    ]
+})  
